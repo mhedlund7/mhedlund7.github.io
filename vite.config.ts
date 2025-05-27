0691f00dfd7d5efd53fad718289ac0849e49162a
mhedlund7 <mhedlund@seas.upenn.edu>
2025-05-27 14:27:48 -0400

    Fix: Update Vite config to use relative paths

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/',  // Root path for username.github.io repository
+  base: './',  // Using relative paths for assets
 })
